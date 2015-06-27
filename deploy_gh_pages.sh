f985f6c9f8299f6ee20ab0a96c59a521609704f8
Jake Goulding <jake.goulding@gmail.com>
2015-06-27 13:15:00 -0400

    Run tests and push site in Travis

---
+++ deploy_gh_pages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eux
+
+if [[ "${TRAVIS_BRANCH}" != 'master' ]] || [[ "${TRAVIS_PULL_REQUEST}" = 'true' ]]; then
+   exit 0
+fi
+
+cd built-site
+
+git init
+git config user.name "Travis-CI"
+git config user.email "builder@travis"
+
+git add .
+git commit -m "Deployed to Github Pages"
+
+set +x # Never print the token!
+git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" master:gh-pages
