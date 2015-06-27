542d548359e0453712eebb216ca905f6b2e60ac0
Jake Goulding <jake.goulding@gmail.com>
2015-06-27 16:00:31 -0400

    Include null checks in the unsafe block

--- examples/slice_arguments/src/lib.rs
+++ examples/slice_arguments/src/lib.rs
@@ -5,8 +5,12 @@ use std::slice;
 
 #[no_mangle]
 pub extern fn sum_of_even(n: *const uint32_t, len: size_t) -> uint32_t {
-    assert!(!n.is_null());
-    let numbers = unsafe { slice::from_raw_parts(n, len as usize) };
+    let numbers = unsafe {
+        assert!(!n.is_null());
+
+        slice::from_raw_parts(n, len as usize)
+    };
+
     let sum =
         numbers.iter()
         .filter(|&v| v % 2 == 0)
--- examples/string_arguments/src/lib.rs
+++ examples/string_arguments/src/lib.rs
@@ -8,10 +8,10 @@ use std::str;
 pub extern fn how_many_characters(s: *const c_char) -> uint32_t {
     let c_str = unsafe {
         assert!(!s.is_null());
-        
+
         CStr::from_ptr(s)
     };
-    
+
     let r_str = str::from_utf8(c_str.to_bytes()).unwrap();
     r_str.chars().count() as uint32_t
 }
