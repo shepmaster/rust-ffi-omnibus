cbb26eb5be23d5a3e831f6906f5fa95ab3e24087
Jake Goulding <jake.goulding@gmail.com>
2015-06-28 15:43:01 -0400

    Add a Makefile function to build shared library names

--- examples/tests.mk
+++ examples/tests.mk
@@ -1,6 +1,6 @@
 ifndef LIB_${d}
 
-LIB_${d}      := ${d}/target/debug/lib${LIB_NAME_${d}}.dylib
+LIB_${d}      := ${d}/target/debug/$(call shared_lib,${LIB_NAME_${d}})
 LIB_DIR_${d}  := ${dir ${LIB_${d}}}
 TEST_DIR_${d} := ${TEST_DIR}/${d}
 
