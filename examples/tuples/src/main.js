050a8d9e6d20cba11c711d57930c70d994b85b40
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-06-30 17:07:22 +0200

    Add node.js implementation of the tuples example

---
+++ examples/tuples/src/main.js
@@ -0,0 +1,16 @@
+var ffi = require('ffi');
+var ref = require('ref');
+var struct = require('ref-struct');
+
+var Tuple = struct({
+    x: 'uint32',
+    y: 'uint32'
+});
+
+var lib = ffi.Library('libtuples', {
+    'flip_things_around': [Tuple, [Tuple]]
+});
+
+var tup = new Tuple({x: 10, y: 20});
+var result = lib.flip_things_around(tup);
+console.log("(%d,%d)", result.x, result.y);
