33381a0fc7068dc78b99fa1d6da599118b7f99f7
Jake Goulding <jake.goulding@gmail.com>
2015-06-30 08:51:03 -0400

    Add basic documentation of the Makefile system

--- examples/tests.mk
+++ examples/tests.mk
@@ -1,7 +1,15 @@
+# This file should be included from a language-specific test file. It
+# defines rules to build the Rust example, as well as variables for
+# shared components. It is safe to be included multiple times in the
+# same directory, but must be included at least once.
+
 ifndef LIB_${d}
 
+# The path to the shared library
 LIB_${d}      := ${d}/target/debug/$(call shared_lib,${LIB_NAME_${d}})
+# The directory containing the shared library
 LIB_DIR_${d}  := ${dir ${LIB_${d}}}
+# The directory to keep example output
 TEST_DIR_${d} := ${TEST_DIR}/${d}
 
 ${LIB_${d}}: DIR := ${d}
--- examples/tests_c.mk
+++ examples/tests_c.mk
@@ -1,17 +1,26 @@
 include ${COMMON_TEST_RULES}
 
+# The value of `d` changes between the time the target is evaluated
+# and the time it is run. To work around this, *target-specific*
+# variables are used.
+
+# Compile the example. All dependencies must be explicitly listed.
 ${d}/c-example: LIB_DIR := ${LIB_DIR_${d}}
 ${d}/c-example: LIB_NAME := ${LIB_NAME_${d}}
 ${d}/c-example: ${d}/src/main.c ${LIB_${d}}
 	clang --std=c11 -o $@ $< -L ${LIB_DIR} -l${LIB_NAME}
 
+# Save the example output into a file by running it
 ${TEST_DIR_${d}}/c-test: LIB_DIR := ${LIB_DIR_${d}}
 ${TEST_DIR_${d}}/c-test: ${d}/c-example ${TEST_DIR_${d}}
 	LD_LIBRARY_PATH=${LIB_DIR} $< > $@
 
+# Compare the example output with the expected output. This does not
+# produce any files, so it is marked `PHONY`.
 .PHONY: c-test_${d}
 c-test_${d}: EXPECTED := ${d}/expected-output
 c-test_${d}: ${TEST_DIR_${d}}/c-test
 	diff -q ${EXPECTED} $<
 
+# Add this example's test target to the global list for this language.
 c: c-test_${d}
