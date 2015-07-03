83a89658b5f8626a64bfcf084cb446c2fa5beab8
Jake Goulding <jake.goulding@gmail.com>
2015-07-03 10:03:55 -0400

    Incorporate language package management into the dependency tree
    This prevents needing to manually re-run the package installer when
    doing local development, allows for parallel installation, and it
    reduces the complexity of the test-time invocation.

--- examples/tests_nodejs.mk
+++ examples/tests_nodejs.mk
@@ -1,7 +1,17 @@
 include ${COMMON_TEST_RULES}
 
+ifndef NODEJS_DEPENDENCIES
+
+NODEJS_DEPENDENCIES := .nodejs-dependencies
+
+${NODEJS_DEPENDENCIES}: package.json
+	npm install
+	touch $@
+
+endif
+
 ${TEST_DIR_${d}}/nodejs-test: LIB_DIR := ${LIB_DIR_${d}}
-${TEST_DIR_${d}}/nodejs-test: ${d}/src/main.js ${TEST_DIR_${d}} ${LIB_${d}}
+${TEST_DIR_${d}}/nodejs-test: ${d}/src/main.js ${TEST_DIR_${d}} ${LIB_${d}} ${NODEJS_DEPENDENCIES}
 	LD_LIBRARY_PATH=${LIB_DIR} node $< > $@
 
 .PHONY: nodejs-test_${d}
--- examples/tests_ruby.mk
+++ examples/tests_ruby.mk
@@ -1,7 +1,21 @@
 include ${COMMON_TEST_RULES}
 
+# System-wide packages need to be installed. We run the appropriate
+# package manager and leave a small cookie file to indicate
+# success. The execution depends on the cookie file.
+
+ifndef RUBY_DEPENDENCIES
+
+RUBY_DEPENDENCIES := .ruby-dependencies
+
+${RUBY_DEPENDENCIES}: Gemfile Gemfile.lock
+	bundle install
+	touch $@
+
+endif
+
 ${TEST_DIR_${d}}/ruby-test: LIB_DIR := ${LIB_DIR_${d}}
-${TEST_DIR_${d}}/ruby-test: ${d}/src/main.rb ${TEST_DIR_${d}} ${LIB_${d}}
+${TEST_DIR_${d}}/ruby-test: ${d}/src/main.rb ${TEST_DIR_${d}} ${LIB_${d}} ${RUBY_DEPENDENCIES}
 	LD_LIBRARY_PATH=${LIB_DIR} bundle exec ruby $< > $@
 
 .PHONY: ruby-test_${d}
