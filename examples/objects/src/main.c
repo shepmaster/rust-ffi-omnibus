9adb18ee1d25a706168b3243fba57b99851f9552
Antoine PLASKOWSKI <antoine.plaskowski@epitech.eu>
2018-11-17 09:33:05 +0100

    Fix C code: printf flags/consistent style/small changes
    - Use correct printf flags for fixed size integer types
    - Use the same style in all C code
    - Small changes that improve code
    
    These changes are primarily motivated by the fact that the actual code
    doesn't use the macro to print fixed size integers. According to the
    standard, you must use these.
    
    I also removed the _S in struct zip_code_database_S, it's redundant.
    
    The style was inconsistent, I stuck to one.

--- examples/objects/src/main.c
+++ examples/objects/src/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <inttypes.h>
 
-typedef struct zip_code_database_S zip_code_database_t;
+typedef struct zip_code_database zip_code_database_t;
 
 extern zip_code_database_t *
 zip_code_database_new(void);
@@ -17,12 +18,12 @@ zip_code_database_population_of(const zip_code_database_t *, const char *zip);
 
 int main(void) {
   zip_code_database_t *database = zip_code_database_new();
-
   zip_code_database_populate(database);
+
   uint32_t pop1 = zip_code_database_population_of(database, "90210");
   uint32_t pop2 = zip_code_database_population_of(database, "20500");
 
   zip_code_database_free(database);
 
-  printf("%d\n", (int32_t)pop1 - (int32_t)pop2);
+  printf("%" PRId32 "\n", (int32_t)pop1 - (int32_t)pop2);
 }
--- examples/tuples/src/main.c
+++ examples/tuples/src/main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <inttypes.h>
 
 typedef struct {
   uint32_t x;
   uint32_t y;
 } tuple_t;
 
-extern tuple_t flip_things_around(tuple_t);
+extern tuple_t
+flip_things_around(tuple_t);
 
 int main(void) {
   tuple_t initial = { .x = 10, .y = 20 };
-  tuple_t new = flip_things_around(initial);
-  printf("(%u,%u)\n", new.x, new.y);
-  return 0;
+  tuple_t result = flip_things_around(initial);
+  printf("(%" PRIu32 ",%" PRIu32 ")\n", result.x, result.y);
 }
